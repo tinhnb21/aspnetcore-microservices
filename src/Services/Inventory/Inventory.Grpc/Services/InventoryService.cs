e5772d584beab74be882113c2554d091bbfe7292
Nguyễn Bá Tình <tinhnb.tech@gmail.com>
2024-07-10 16:03:44 +0700

    commit

---
+++ src/Services/Inventory/Inventory.Grpc/Services/InventoryService.cs
@@ -0,0 +1,32 @@
+﻿using Grpc.Core;
+using Inventory.Grpc.Protos;
+using Inventory.Grpc.Repositories.Interfaces;
+
+namespace Inventory.Grpc.Services
+{
+    public class InventoryService : StockProtoService.StockProtoServiceBase
+    {
+        private readonly IInventoryRepository _repository;
+        private readonly Serilog.ILogger _logger;
+
+        public InventoryService(IInventoryRepository repository, Serilog.ILogger logger)
+        {
+            _repository = repository ?? throw new ArgumentNullException(nameof(repository));
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+
+        public override async Task<StockModel> GetStock(GetStockRequest request, ServerCallContext context)
+        {
+            _logger.Information($"BEGIN Get Stock of ItemNo: {request.ItemNo}");
+            var stockQuantity = await _repository.GetStockQuantity(request.ItemNo);
+            var result = new StockModel
+            {
+                Quantity = stockQuantity,
+            };
+
+            _logger.Information($"END Get Stock of ItemNo: {request.ItemNo} - Quantity: {result.Quantity}");
+
+            return result;
+        }
+    }
+}
